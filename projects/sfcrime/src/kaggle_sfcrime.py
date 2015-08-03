ab7f4757d590e85f3a10876ca21d3736aa16a042
dmuralik <dineshat@gmail.com>
2015-08-03 12:16:15 -0500

    created src and moved file to be under src

--- projects/sfcrime/src/kaggle_sfcrime.py
+++ projects/sfcrime/src/kaggle_sfcrime.py
@@ -1,11 +1,17 @@
+#This is for a Kaggle datascience competition. More information about it can be found here: https://www.kaggle.com/c/sf-crime/data
+#I have used a RandomForest classifier for prediction.
 import pandas as pd
 import patsy
 from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import time
+from sklearn import cross_validation
+import scipy as sp
 
+#to track the performance
 startTime = time.time()
 
+#a function that can mungle the data (for reusability)
 def wrangle(dataFrame, isTraining):
     dayOfWeekChanged = pd.get_dummies(dataFrame['DayOfWeek'])
     pdDistrictChanged = pd.get_dummies(dataFrame['PdDistrict'])
@@ -30,20 +36,29 @@ test = pd.read_csv("./data/test.csv",
                    usecols = ['DayOfWeek','PdDistrict','X','Y']
                    )
 
+#unindexes class labels to descriptions
 def unIndex(labels, enumIndex):
     return [labels[index] for index in enumIndex]
 
 trainingWrangled, enumIndexTraining, categoryLabelsTraining = wrangle(train, True)
 testWrangled, enumIndexTest, categoryLabelsTest = wrangle(test, False)
+
 rf = RandomForestClassifier(n_estimators=100)
-rf.fit(trainingWrangled, enumIndexTraining)
-predicted = rf.predict(testWrangled)
-dfWithClass = pd.DataFrame(predicted, columns = ['Class'])
-final = pd.concat([testWrangled, dfWithClass], axis=1)
-classLabels = unIndex(categoryLabelsTraining, final['Class'])
-categoriesPredicted = pd.get_dummies(classLabels)
-categoriesPredicted.to_csv("./data/predictions.csv")
-print("Time taken:%.1f" % (time.time() - startTime))
+cv = cross_validation.KFold(len(train), n_folds=5, indices=False)
+results = []
+for traincv, testcv in cv:
+    probas = rf.fit(trainingWrangled[traincv], enumIndexTraining[traincv]).predict_proba(trainingWrangled[testcv])
+    results.append(metrics.log_loss(enumIndexTraining[testcv], [x[1] for x in probas]))
+
+print(results)
+#rf.fit(trainingWrangled, enumIndexTraining)
+#predicted = rf.predict(testWrangled)
+#dfWithClass = pd.DataFrame(predicted, columns = ['Class'])
+#final = pd.concat([testWrangled, dfWithClass], axis=1)
+#classLabels = unIndex(categoryLabelsTraining, final['Class'])
+#categoriesPredicted = pd.get_dummies(classLabels)
+#categoriesPredicted.to_csv("./data/predictions.csv")
+print("Time taken:%.1f seconds" % (time.time() - startTime))
 
 
 
